8763c2453ee1e5b828c7069a007b16c549fba595
Christian Esqueda <christian.esqueda88@gmail.com>
2021-01-14 20:14:29 -0800

    remove submodule

---
+++ client/src/models/user.js
@@ -0,0 +1,9 @@
+export default class User {
+    constructor(username, firstName, lastName, email, password) {
+      this.username = username;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.email = email;
+      this.password = password;
+    }
+  }
